06df1f1750c3eefdc947cd027f046fdb31c62af6
martins obayomi <obayomimartins96@gmail.com>
2020-02-23 16:35:37 +0100

    Setup webpack

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar.jsx';
+
+const LandingPage = () => (
+  <div>
+    <Navbar />
+    <h1>Landing page</h1>
+  </div>
+);
+
+export default LandingPage;
